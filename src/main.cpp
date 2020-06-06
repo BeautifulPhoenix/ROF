0eb8ff14099971ce74cd31eff87d18d566ee43c1
MrRayBen <40451847+MrRayBen@users.noreply.github.com>
2020-06-06 14:01:17 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <stdlib.h>
+#include <iostream>
 #include <stdio.h>
 #include <time.h>
+#include "funciones.c"
+#include "funcions.h"
 struct Panel
 {
 	int valor;
@@ -16,19 +19,22 @@ struct Jugador Jugador[10];
 int main(int argc, char *argv[])
 {
 	int choise;
+	int panel;
+	int panel2;
 	int var = 0;
 	int lleno = 0;
-	struct panel aux;
+	char ruleta [12][1];
 	do
 	{
 
 
-        std::cout << "1. Insertar Jugador" << std::endl;
-        std::cout << "2. Mostrar Jugadores " << std::endl;
+        std::cout << "1. Insertar Concursantes" << std::endl;
+        std::cout << "2. Preparar Panells" << std::endl;
 		std::cout << "3. Inicializar Partida" << std::endl;
 		std::cout << "4. Jugar!" << std::endl;
+		std::cout << "0. Salir" << std::endl;
+		std::cin >> choise;
 
-		scanf ("%d",&choise);
 		switch (choise)
 		{
 		case 1:
@@ -37,6 +43,28 @@ int main(int argc, char *argv[])
         break;
 
 		case 2:
+
+		do
+		{
+
+		
+		std::cout << "1. Carregar Dades del fitxer/per defecte" << std::endl;
+		std::cout << "2. Introduir panell nou" << std::endl;
+		std::cout << "3. Mostrar panells" << std::endl;
+		std::cout << "4. Tornar" << std::endl;
+
+		switch (panel2)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3: 
+			break;
+		case 4:
+			break;
+		}
+		} while(panel !=0);
 
 		
         break;

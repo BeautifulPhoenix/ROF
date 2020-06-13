7e4f324e31e990caad9ced647473110c48aa0788
MrRayBen <40451847+MrRayBen@users.noreply.github.com>
2020-06-13 16:59:41 +0200

    Massive update

--- src/funciones.cpp
+++ src/funciones.cpp
@@ -10,9 +10,11 @@
 using namespace std;
 
 string panel[15];
+string nwpanel[15];
 string rpanel[2]={"Estoy encerrado "};
 string spanel[30]=/*{"ESTOYENCERRADO"};*/{"E","S", "T", "O", "Y", "E", "N", "C","E", "R", "R", "A", "D","O", "\0"};
 string Pista [2]={"Confinamiento"};
+string pistap;
 string ruleta [28][1]= {"25", "50", "75", "25", "50", "75", "25", "50", "75", "25", "50", "75", "25", "50", "75", "25", "50", "75", "100", "150", "200", "100", "150", "Bancarrota", "Comodin", "Todas las vocales", "X2", "1/2"};
 string ruleta2 [28]{"25", "50", "75", "25", "50", "75", "25", "50", "75", "25", "50", "75", "25", "50", "75", "25", "50", "75", "100", "150", "200", "100", "150"};
 string solucion [30];
@@ -20,6 +22,8 @@ string solucion [30];
 int num = rand() % 28; 
 int choise;
 int dado= rand() % 2;
+int cnt;
+
 int i;
 int z;
 int comodin;
@@ -29,6 +33,7 @@ string cp;
 
 int eur=0;
 bool encontrado=false;
+int g;
 
 
 
@@ -76,18 +81,20 @@ int npanel(int valor, int frase){
 
 
     // La primera posicion a introducir no funciona de 5 -> solo 4 
-    int cnt;
-    string npanel[15];
-    std::cout << "Cuantos paneles quieres introducir?" << std::endl;
-    cin >> cnt;
-    
-    
+
+    std::cout << "Dime La longitud del Panel" << std::endl;
+    cin >>cnt;
+
+std::cout << "Dime El panel k kieres introducir letra por letra" << std::endl;
     for (size_t i = 0; i < cnt; i++)
     {
-    std::cout << "Dime los nuevos panels a introducir:" << std::endl;
+    std::cout << "Dime el nuevos panel a introducir:" << std::endl;
     std::getline(std::cin, panel[i]);
     }
     
+
+std::cout << "Dime La pista del panel" << std::endl;
+cin >> pistap;
 
 
 
@@ -179,10 +186,7 @@ int partida(int valor, int frase){
     struct concursante concursante;
 
 
-    std::cout << concursante.nom <<" Bienvenido a la Ruleta de la Suerte " <<std::endl;
-
-    cout << "La Pista Es: " <<endl;
-    std::cout << " "<< Pista[dado] << std::endl;
+  
 
 
    /* num = rand() % 28; 
@@ -191,15 +195,518 @@ int partida(int valor, int frase){
     //int c = getchar();
     std::cout << "...... te a tocado la posicion: " << num<< std::endl;
     std::cout << " Te a tocado: " << ruleta[num][1] <<"€"<< std::endl;*/
+
 do
 {
-   /*   if (num == 22)
-    {
-        choise==0;
-    } */
+
+        cout<<"====================================================="<<endl;
+        std::cout << "1. Iniciar Juego con el Panel Personalizado" << std::endl;
+		std::cout << "2. Iniciar Juego con el Panel por Defecto" << std::endl;
+		std::cout << "0. Salir " << std::endl;
+        cout<<"====================================================="<<endl;
+
+cin>>g;
+switch (g)
+
+
+
+
+{
+case 1:
+    do
+{
+  
     
 
     cout <<"Que quieres hacer?"<< endl;
+    std::cout << concursante.nom <<" Bienvenido a la Ruleta de la Suerte Personalizada" <<std::endl;
+
+    cout << "La Pista Es: " <<endl;
+    std::cout << " "<< pistap << std::endl;
+        
+  
+        
+        cout<<"====================================================="<<endl;
+        std::cout << "1. Girar Ruleta" << std::endl;
+		std::cout << "2. Comprar letras" << std::endl;
+        std::cout << "3. Todas las Vocales" << std::endl;
+		std::cout << "0. Salir " << std::endl;
+        
+		cout<<"====================================================="<<endl;
+               // cout <<"Panel: "<< spanel[i] <<endl;
+            cout << "Saldo: "<< concursante.dinero <<"€"<<endl;
+        std::cout << "Comodines:" << comodin<< std::endl;
+        cout << "Pista: "<< Pista[0] <<endl;
+        cout << "Panel:" <<endl;
+         cout << "  _ _ _ _ _ _ _ _ _ _ _ _ _ _"<<endl;
+        //cout <<"Solucion:  "<< solucion[i] <<endl;
+         for (size_t y= 0; y< 30; y++)
+    {
+
+        cout << " "<< solucion[y]; 
+
+    }//cout << " v" << i;
+
+    cout << endl;
+
+       i=i+1;
+		std::cin >> choise;
+        switch (choise)
+        {
+
+        case 1:
+    
+        num = rand() % 28; 
+
+
+    std::cout << "Giras la Ruleta" << std::endl;
+    //int c = getchar();
+    std::cout << "...... te a tocado la posicion: " << num<< std::endl;
+    std::cout << " Te a tocado: " << ruleta[num][1] <<" €"<< std::endl; 
+    //if para contabilizar el comodin
+    if (num !=23)
+    {
+        //comodin=comodin=1;
+
+    }
+    else
+    {
+        comodin=comodin+1;
+    }
+    
+    //23 if + el X2 y el 1/2 para hacer funcinar el sistema de monetizacion comenzamos 
+    // desde la posicion 0
+    if (num !=0)
+    {
+
+
+    }
+    else
+    {
+    
+    eur=eur+50;
+        
+    }
+    
+
+
+     if (num !=1)
+    {
+        
+    }
+    else
+    {
+
+        eur=eur+75;
+        
+    }
+
+    
+
+        if (num !=2)
+    {
+        
+    }
+    else
+    {
+
+        eur=eur+25;
+        
+    }
+
+        if (num !=3)
+    {
+        
+    }
+    else
+    {
+        eur=eur+50;
+        
+    }
+    
+   
+
+        if (num !=4)
+    {
+        
+        
+    }
+    else
+    {
+
+        eur=eur+75;
+        
+    }
+
+     
+
+        if (num !=5)
+    {
+        
+    }
+    else
+    {
+        eur=eur+25;
+        
+    }
+
+    
+
+        if (num !=6)
+    {
+        
+    }
+    else
+    {
+        eur=eur+50;
+
+        
+    }
+
+   
+
+        if (num !=7)
+    {
+        
+    }
+    else
+    {
+       eur=eur+75; 
+    }
+     
+
+        if (num !=8)
+    {
+        
+    }
+    else
+    {
+
+        eur=eur+25;
+        
+    }
+    
+
+        if (num !=9)
+    {
+        
+    }
+    else
+    {
+
+        eur=eur+50;
+        
+    }
+
+        if (num !=10)
+    {
+        
+    }
+    else
+    {
+        eur=eur+75;
+        
+    }
+
+        if (num !=11)
+    {
+        
+    }
+    else
+    {
+        eur=eur+25;
+    }
+
+        if (num !=12)
+    {
+        
+    }
+    else
+    {
+        eur=eur+50;        
+    }
+
+        if (num !=13)
+    {
+        
+    }
+    else
+    {
+        eur=eur+75;
+        
+    }
+
+        if (num !=14)
+    {
+        
+    }
+    else
+    {
+        eur=eur+25;
+    }
+
+        if (num !=15)
+    {
+
+    }
+    else
+    {
+        eur=eur+50;
+
+    }
+
+        if (num !=16)
+    {
+
+    }
+    else
+    {
+         eur=eur+75;
+
+    }
+
+        if (num !=17)
+    {
+        
+    }
+    else
+    {
+        eur=eur+100;
+    }
+
+        if (num !=18)
+    {
+        
+    }
+    else
+    {
+        eur=eur+150;
+    }
+
+        if (num !=19)
+    {
+        
+    }
+    else
+    {
+        eur=eur+200;
+    }
+
+        if (num !=20)
+    {
+        
+    }
+    else
+    {
+        eur=eur+100;
+    }
+
+        if (num !=21)
+    {
+        
+    }
+    else
+    {
+        eur=eur+21;
+    }
+
+        if (num !=25)
+    {
+        
+    }
+    else
+    {
+        eur=eur*2;
+    }
+
+    if (num !=26)
+    {
+        /* code */
+    }
+    else
+    {
+        eur=eur/2;
+    }
+
+    if (num != 27)
+    {
+        
+    }
+    else
+    {
+        eur=eur+25;
+    }
+    if (num !=24)
+    {
+        /* code */
+    }
+    else
+    {
+
+        vocal=1;
+
+    }
+    
+    
+    
+    concursante.dinero=eur;
+
+    
+    
+    
+         
+
+
+        break;
+
+        case 2:
+  
+    
+    /*
+    //cout << "En que posicion quieres la letra a comprar?"<<endl;
+
+    //cin >> z;
+    cout<< "Que letra Quieres comprar?"<<endl; 
+
+        cin >> cp;
+
+        // lo suyo seria hacer un bucle de tal manera que pillara cualquier letra de cualquier posicion pero al hacerlo con el for el programa peta
+
+        for (size_t z = 0; z < 14; z++)
+        {
+    
+            if(spanel[z].find(cp) > 0){
+            cout <<" Esa letra NO existe en el panel GAME OVER"<<endl;
+            if (comodin !=1)
+            {
+                                num =22;
+
+            }
+            else
+            {
+            std::cout << "Has perdido pero tienes comodin " << std::endl;
+
+            comodin=0;
+
+            } 
+        }
+            else
+            {
+
+        std::cout << "Letra CORRECTA! " << spanel[z] << std::endl;
+
+
+        solucion[z]=cp;
+                     }
+
+        }  
+*/
+
+
+cout << "Dime  la Letra " <<endl;
+cin >> cp;
+
+encontrado=0;
+
+for (size_t z = 0; z < 14; z++)
+{
+
+if (panel[z]==cp)
+{
+    encontrado=1;
+    solucion[z]=cp;
+}
+
+if (encontrado==1)
+{
+    std::cout << "Letra CORRECTA!" << std::endl;
+}else
+{
+std::cout << "Esa letra NO existe en el panel." << std::endl;
+if (comodin==0)
+{
+
+std::cout << "GAME OVER" << std::endl;
+
+num=22;
+
+}else
+{
+    std::cout << "Has perdido el comodin" << std::endl;
+    comodin=0;
+}
+
+
+}
+
+
+
+
+}
+
+        break;
+        
+        
+        case 3:
+
+        // Todas las Vocales del 1r panel
+        // solo esta configurado para el primer panel 
+    if (vocal != 1)
+    {
+    std::cout << "" << std::endl;
+    std::cout << "No me seas tramposo no te a tocado el comodin" << std::endl;
+std::cout << "" << std::endl;
+ 
+            }
+    else
+    {
+        solucion[0]={"E"};
+        solucion[3]={"O"};
+        solucion[5]={"E"};
+        solucion[8]={"E"};
+        solucion[11]={"A"};
+        solucion[13]={"O"};
+    }
+    
+        break;      
+        default:
+            break;
+        }
+} while (choise !=0 /*&& num !=22*/);
+
+/*if (num != 22)
+{
+
+    //el primer COUT no se acctiva  solo se activa el segundo cuando sale num=22 es decir Bancarrota
+
+    std::cout << "Que lastima... has perdido GAME OVER volviendo al Menu" << std::endl;
+
+}
+
+else
+{
+    std::cout << "" << std::endl;
+    std::cout << "Que lastima... has perdido GAME OVER volviendo al Menu" << std::endl;
+    std::cout << "" << std::endl;
+}*/
+    break;
+
+default:
+    break;
+    
+
+      case 2:
+
+        do
+{
+  
+    
+
+    cout <<"Que quieres hacer?"<< endl;
+    std::cout << concursante.nom <<" Bienvenido a la Ruleta de la Suerte " <<std::endl;
+
+    cout << "La Pista Es: " <<endl;
+    std::cout << " "<< Pista[dado] << std::endl;
         
   
         
@@ -594,7 +1101,7 @@ if (spanel[z]==cp)
     solucion[z]=cp;
 }
 
-if (encontrado=1)
+if (encontrado==1)
 {
     std::cout << "Letra CORRECTA!" << std::endl;
 }else
@@ -612,57 +1119,10 @@ num=22;
     std::cout << "Has perdido el comodin" << std::endl;
     comodin=0;
 }
-
-
+}
 }
 
 
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
         break;
         
         
@@ -708,6 +1168,29 @@ else
     std::cout << "Que lastima... has perdido GAME OVER volviendo al Menu" << std::endl;
     std::cout << "" << std::endl;
 }
+        
+        break;
+}
+
+}while (choise !=0 && num !=22);
+
+if (num != 22)
+{
+
+    //el primer COUT no se acctiva  solo se activa el segundo cuando sale num=22 es decir Bancarrota
+
+    std::cout << "Que lastima... has perdido GAME OVER volviendo al Menu" << std::endl;
+
+}
+
+else
+{
+    std::cout << "" << std::endl;
+    std::cout << "Que lastima... has perdido GAME OVER volviendo al Menu" << std::endl;
+    std::cout << "" << std::endl;
+};
+
+
 
 
 
